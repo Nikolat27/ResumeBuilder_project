2c5c07083bc3961c755b8ba08418df150b44b77e
Nikolat27 <samalizadeh899@gmail.com>
2024-10-16 23:56:22 +0330

    My template is almost done ill do the backend first

--- resume_app/urls.py
+++ resume_app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 app_name = "resume_app"
 urlpatterns = [
-    path("creation", views.resume_creation, name="resume_creation")
+    path("creation", views.ResumeCreationView.as_view(), name="resume_creation")
 ]
--- resume_app/views.py
+++ resume_app/views.py
@@ -1,7 +1,21 @@
+import json
+
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 
+@method_decorator(csrf_exempt, name='dispatch')
+class ResumeCreationView(View):
+    template_name = 'resume_app/resume_creation.html'
 
-def resume_creation(request):
-    return render(request, "resume_app/resume_creation.html")
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        data = json.loads(request.body)
+        
